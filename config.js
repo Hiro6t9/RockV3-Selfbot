5fe5df58a612c2fd860464a39e3e5fbf89844f8d
Hiro <hiroyt1137@gmail.com>
2025-04-29 18:12:06 +0600

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     prefix: "+", // Set your command prefix
     allowedUserIDs: "1198187445859139595", // Replace with actual user IDs
     allowedNoPrefixUserIDs: "1198187445859139595", // Users allowed to use commands without a prefix
-    applicationId: '', // Add your target bot ID here(required for status to work.)
+    applicationId: '1356061076952846481', // Add your target bot ID here(required for status to work.)
 
     // Enable or disable the web server
     enableWebServer: true, // Set to false to disable the web server(turn it false if running this on a host.)
